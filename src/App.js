4dbac691b96fbb1fcc008f9842d7768a5c8d9e47
kriwis <kwsvendsen@gmail.com>
2022-01-24 14:07:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { CardList } from './Components/Card-list/card-list.component';
+import { SearchBox } from './Components/search-box/searchbox.component';
+import './App.css';
+
+class App extends Component {
+   
+  constructor () {
+    super();
+  
+    this.state = {
+    monsters: [],
+    searchField: ''
+    };
+  } // end constructor
+  
+  //fetch data on mount
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}));
+}
+
+onSearchChange = event => {
+  this.setState({ searchField: event.target.value });
+};
+  
+    render() {
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+  return (
+    
+        <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>  
+        
+  
+   
+  );
+   }
+}
+
+export default App;
